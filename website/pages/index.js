7d210c782e29d4570b7bd8cd07cfb8aeaae0c1c8
Sergio Regueira <sergio.regueira@arengu.com>
2019-03-11 14:17:47 +0100

    Init Docusaurus

---
+++ website/pages/index.js
@@ -0,0 +1,26 @@
+const React = require('react');
+
+class Index extends React.Component {
+  render() {
+    const {config: siteConfig, language = ''} = this.props;
+
+    return (
+      <div>
+        <div className="homeContainer">
+          <div className="homeSplashFade">
+            <div className="wrapper homeWrapper">
+              <div className="inner">
+                <h2 className="projectTitle">
+                  {siteConfig.title}
+                  <small>{siteConfig.tagline}</small>
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
---
+++ website/siteConfig.js
@@ -0,0 +1,67 @@
+const siteConfig = {
+  title: 'Arengu Docs',
+  tagline: 'User onboarding made simple',
+  url: 'https://docs.arengu.com',
+  baseUrl: '/',
+
+  docsUrl: '',
+
+  // Used for publishing and more
+  projectName: 'documentation',
+  organizationName: 'arengu',
+
+  headerLinks: [
+    {doc: 'product/index', label: 'Product'},
+  ],
+
+  /* path to images for header/footer */
+  headerIcon: 'img/docusaurus.svg',
+  footerIcon: 'img/docusaurus.svg',
+  favicon: 'img/favicon.png',
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#2E8555',
+    secondaryColor: '#205C3B',
+  },
+
+  /* Custom fonts for website */
+  /*
+  fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },
+  */
+
+  copyright: `Copyright © ${new Date().getFullYear()} Arengu Software`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: [],
+
+  onPageNav: 'separate',
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: 'img/docusaurus.png',
+  twitterImage: 'img/docusaurus.png',
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+};
+
+module.exports = siteConfig;
